53ff24f332078d13a9d767a77cabd7594129c895
Masashi <massanndesu52@gmail.com>
2025-12-11 15:19:58 +0900

    add: userの機能追加

--- models/user.py
+++ models/user.py
@@ -4,6 +4,8 @@ from .db import db
 class User(Model):
     name = CharField()
     age = IntegerField()
+    email = CharField(null=True)
+    address = CharField(null=True)
 
     class Meta:
         database = db
